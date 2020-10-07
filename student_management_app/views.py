1b12d68937ec26f1431c3b9bc20eba3fa3bc4bca
Orlando Dilmar Gareca Peña <orlando.dilmar.gareca.pena@gmail.com>
2020-10-07 15:04:32 -0400

    Login and Logout

---
+++ student_management_app/views.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import  login, logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+from student_management_app.EmailBackEnd import EmailBackEnd
+
+
+def shwDemoPage(request):
+    return render(request, "demo.html")
+
+
+def ShowLoginPage(request):
+    return render(request, "login_page.html")
+
+
+def doLogin(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method not Allowed</h2>")
+    else:
+        user = EmailBackEnd.authenticate(request, username=request.POST.get("email"),password=request.POST.get("password"))
+        if user != None:
+            login(request,user)
+            return HttpResponse("Email : " + request.POST.get("email") + "Password : " + request.POST.get("password"))
+        else:
+            return HttpResponse(request,"Inicio de Sesion Invalido")
+
+
+def GetUserDetails(request):
+    if request.user != None:
+        return HttpResponse("User :" + request.user.email + " usertype : " + request.user.user_type)
+    else:
+        return HttpResponse("Por Favor inicie sesion primero")
+
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student_management_app'
 ]
 
 MIDDLEWARE = [
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -81,6 +79,17 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'student_management_system',
+#         'USER': 'postgres',
+#         'PASSWORD': 'malaleche',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -100,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -114,8 +122,15 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+AUTH_USER_MODEL = "student_management_app.CustomUser"
+AUTHENTICATION_BACKENDS=['student_management_app.EmailBackEnd.EmailBackEnd']
