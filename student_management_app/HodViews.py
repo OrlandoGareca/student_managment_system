048d17467940394e617123a3003806245dc3df99
OrlandoGareca <orlando.dilmar.gareca.pena@gmail.com>
2020-11-12 18:48:58 -0400

    Feedback reply of student and staff in admin send response

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -8,7 +8,8 @@ from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 from student_management_app.forms import AddStudentForm, EditStudentForm
-from student_management_app.models import CustomUser, Courses, Subjects, Staffs, Students, SessionYearModel
+from student_management_app.models import CustomUser, Courses, Subjects, Staffs, Students, SessionYearModel, \
+    FeedBackStudent, FeedBackStaffs
 
 
 def admin_home(request):
@@ -351,6 +352,7 @@ def check_email_exist(request):
     else:
         return HttpResponse(False)
 
+
 @csrf_exempt
 def check_username_exist(request):
     username = request.POST.get("username")
@@ -359,3 +361,38 @@ def check_username_exist(request):
         return HttpResponse(True)
     else:
         return HttpResponse(False)
+
+
+def student_feedback_message(request):
+    feedbacks = FeedBackStudent.object.all()
+    return render(request, "hod_templates/student_feedback_template.html", {"feedbacks": feedbacks})
+
+
+def staff_feedback_message(request):
+    feedbacks = FeedBackStaffs.object.all()
+    return render(request, "hod_templates/staff_feedback_template.html", {"feedbacks": feedbacks})
+
+
+@csrf_exempt
+def student_feedback_message_replied(request):
+    feedback_id = request.POST.get("id")
+    feedback_message = request.POST.get("message")
+    try:
+        feedback = FeedBackStudent.object.get(id=feedback_id)
+        feedback.feedback_reply = feedback_message
+        feedback.save()
+        return HttpResponse("True")
+    except:
+        return HttpResponse("False")
+
+@csrf_exempt
+def staff_feedback_message_replied(request):
+    feedback_id = request.POST.get("id")
+    feedback_message = request.POST.get("message")
+    try:
+        feedback = FeedBackStaffs.object.get(id=feedback_id)
+        feedback.feedback_reply = feedback_message
+        feedback.save()
+        return HttpResponse("True")
+    except:
+        return HttpResponse("False")
